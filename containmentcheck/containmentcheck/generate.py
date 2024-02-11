bf6eb22ef6fcc750689d4023c89ab27d540def67
Jacob-Allebach <allebach01@allegheny.edu>
2024-02-11 11:00:09 -0500

    Add duplication functionality to `generate.py`

--- containmentcheck/containmentcheck/generate.py
+++ containmentcheck/containmentcheck/generate.py
@@ -15,12 +15,20 @@ def generate_random_container(
     size: int,
     maximum: int,
     make_tuple: bool = False,
+    num_duplicates: int = 0
 ):
     """generate a list of random values for a specific size
     and with a number up to a specific maximum"""
     random_list = []
+    if num_duplicates != 0:
+        duplicate = size/num_duplicates
+    else:
+        duplicate = 0
     for i in range(size):
-        random_list.append(generate_random_number(maximum, False))
+        if duplicate != 0 and i % duplicate == 0:
+            random_list.append(generate_random_number(maximum, True))
+        else:
+            random_list.append(generate_random_number(maximum, False))
     # convert the list to a tuple if this was requested
     if make_tuple:
         return tuple(random_list)
